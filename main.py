7bb76c553b7202ac9f2b8431b51d8e9598694cfb
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-09-19 17:52:45 -0600

    Basic specs to code
    Commenting BASE phone commands that will be used and tested religously.

--- main.py
+++ main.py
@@ -25,6 +25,17 @@ import random
 import string
 import csv
 
+# List of BASE phone commands (the foundation of this project)
+# ------ At any instance, the phone might be referenced as the phone number or last name ------
+# - phone dials another phone
+# - phone goes offhook
+# - phone goes onhook
+# - phone is transfered to another phone
+# - phone is put into a conference call
+# - list status of phone (onhook, offhook, dialing, ringing))
+
+
+
 def main():
     pass
 
