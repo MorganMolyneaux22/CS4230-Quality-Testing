23737dd30780b42c7ea8ac4d24bf192685ee2fed
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-09-19 17:46:54 -0600

    Fake phone book data
    Phone book containing dummy data.

--- main.py
+++ main.py
@@ -21,8 +21,15 @@
 ################################################################################
 
 
-def main():
-    print("Hello world")
+import random
+import string
+import csv
 
+def main():
+    pass
+
+# Generate and write to file
 if __name__ == "__main__":
-    main()
+    # pathway to the .csv containing the phonebook with fake data. 
+    file_name = "phonebook.csv"
+   
