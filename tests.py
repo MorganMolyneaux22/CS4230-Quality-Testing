a27ba20d0ea4a52860e77b77df3544b1fff5459e
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-09-19 18:40:26 -0600

    Brainstorm

---
+++ tests.py
@@ -0,0 +1,17 @@
+# We will want about 9-49 test cases for this?
+# Need to to find if it for each class, or overall. 
+
+
+
+# Boundary Testing 
+# Create a boundary test to find what the boundary might be 
+    # - example, if the phone number is 10 digits, what happens if it is 9 digits?
+    # - example, if the phone number is 10 digits, what happens if it is 11 digits?
+    # - example, if the phone is on hook, what happens if it is off hook?
+    # - example, if the phone is off hook, what happens if it is on hook?
+    # - example, if the phone is dialing, what happens if it is ringing?
+    # - example, if the phone is ringing, what happens if it is dialing?
+    
+# These are all hypotheticals, but I'm just writing them down while they're fresh on my head.  
+# Also to give an idea of what it's asking, so we can think of better ones later. 
+
